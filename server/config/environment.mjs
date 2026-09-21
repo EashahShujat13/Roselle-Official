18c28028b0b741ea946198f7a984a41642d5c0e7
EashahShujat13 <eashahofficework@gmail.com>
2026-09-21 23:53:38 +0500

    FIxing Errors

--- server/config/environment.mjs
+++ server/config/environment.mjs
@@ -3,9 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const MONGO_URI = process.env.MONGO_URI;
-export  const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_SECRET = process.env.JWT_SECRET;
 export const PORT = process.env.PORT ;
 export const EMAIL = process.env.EMAIL;
 export const APP_PASSWORD = process.env.APP_PASSWORD;
 export const FRONTEND_URL = process.env.FRONTEND_URL;
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
